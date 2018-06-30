3093a53c9ef22a90cc160fe630974cf28a872f6d
Priyansh Balyan <priyanshbalyan@gmail.com>
2018-06-30 15:59:36 +0530

    login and registeration connected to sql database

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ let bodyParser = require('body-parser');
 let multer = require('multer');
 let upload = multer();
 
+let passport = require('passport');
+
 //for parsing application/xwww
 app.use(bodyParser.urlencoded({ extended: true }));
 //for parsing application/json
@@ -17,6 +19,9 @@ app.use(express.static('css'));
 app.use(express.static('img'));
 app.use(express.static('myfont'));
 app.use('/usertemplate', express.static('usertemplate'));
+app.use('/login.html', express.static('login.html'));
+app.use('/register.html', express.static('register.html'));
+app.use('/reset.html', express.static('reset.html'));
 
 //Connecting to local database
 const sqlite3 = require('sqlite3').verbose();
@@ -25,17 +30,36 @@ let db = new sqlite3.Database('database.db', err => {
     console.log('Connected to database.db SQLite3 Local Database');
     db.exec(`
         CREATE TABLE IF NOT EXISTS Users ( 
-        contact_id integer PRIMARY KEY,
-        first_name text NOT NULL,    
-        last_name text NOT NULL,    
-        address_line_1 text NOT NULL,
-        address_line_2 text NOT NULL,
-        city text NOT NULL,
-        pincode text NOT NULL,
-        email text NOT NULL   
+            contact_id integer PRIMARY KEY,
+            first_name text NOT NULL,
+            last_name text NOT NULL,
+            email varchar(100) NOT NULL,    
+            password text NOT NULL,
+            referral text
         );
     `);
 });
+
+// passport.use(new LocalStrategy((username, password, done) => {
+//     db.get('SELECT password FROM users WHERE username = ?', username, (err, row) => {
+//         if (!row) return done(null, false);
+//         db.get('SELECT username, id FROM users WHERE username = ? AND password =? ', username, password, (err, row) => {
+//             if (!row) return done(null, false);
+//             return done(null, row);
+//         })
+//     });
+// }));
+
+// passport.serializeUser((user, done) => {
+//     return done(null, user.id);
+// });
+
+// passport.deserializeUser((id, done) => {
+//     db.get('SELECT id, username FROM users WHERE id = ?', id, (err, row) => {
+//         if (!row) return done(null, false);
+//         return done(null, row);
+//     });
+// });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('listening on : ' + (process.env.PORT || 3000));
@@ -45,11 +69,13 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
 });
 
+
+
 app.post('/account', (req, res) => {
     console.log(req.body);
     data = req.body;
     db.exec(`
-    INSERT INTO Users(first_name, last_name, address_line_1, address_line_2, city, pincode, email)
+    INSERT INTO Users(first_name, last_name, email, password, )
     VALUES(` + Object.values(data).join(",") + `);
     `);
     res.send(data);
@@ -67,6 +93,39 @@ app.post('/updatekyc', (req, res) => {
     res.send(data);
 });
 
+app.post('/register', (req, res) => {
+    data = req.body;
+    if (data.password !== data.cpassword) return res.send({ error: "Passwords dont match" });
+    delete data.cpassword;
+    data.email = 'ajd';
+    sql = "INSERT INTO Users(contact_id, first_name, last_name, email, password, referral) VALUES(" + Date.now() + ",'" + Object.values(data).join("', '") + "');";
+    //console.log(sql);
+    db.exec(sql, err => {
+        if (err) return res.send(err);
+        res.cookie('userid', data.first_name + data.email);
+        return res.redirect('/usertemplate/account.html');
+    });
+    //res.redirect('/');
+});
+
+app.post('/login', (req, res) => {
+    data = req.body;
+    console.log(data);
+    //sql = "SELECT first_name, last_name, email, referral, contact_id FROM Users WHERE email = '" + data.email + "' AND password = '" + data.password + "'";
+    sql2 = "SELECT first_name, last_name, email, referral, contact_id FROM Users WHERE email = 'dummycontent@gmail.com' AND password = '1234'";
+    //console.log(sql2);
+    db.get(sql2, (err, row) => {
+        console.log("function trigger");
+        if (!row) {
+            console.log("0 rows found");
+            return res.send('Error2');
+        }
+        console.log(row);
+        return res.send('test');
+    });
+    //res.send(req.body);
+
+});
 
 app.get('*', (req, res) => {
     res.send('404 Page Not Found.');
